3e1a629722caac448f62b99ac94ee8de0e97bee1
Shettynik <nikshitashetty2612@gmail.com>
2021-02-05 00:09:44 +0530

    modify

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 5000,
-  //MONGODB_URL: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}?retryWrites=true&w=majority` || 'mongodb://localhost/EKart',
-  MONGODB_URL: "mongodb://localhost/rancholabs",
+  MONGODB_URL: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}?retryWrites=true&w=majority` || 'mongodb://localhost/EKart',
+  //MONGODB_URL: "mongodb://localhost/rancholabs",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
   GCLIENT_ID: process.env.GCLIENT_ID || "GoogleClientId",
   FB_TOKEN: process.env.FB_TOKEN || "FacebookToken",
